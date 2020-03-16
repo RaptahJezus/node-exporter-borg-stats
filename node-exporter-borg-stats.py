973e9023397a4410517fd3cd14515230979879cf
Daniel Blazejewski <dan@backups01.home.danielblazejewski.com>
2020-03-16 13:51:59 -0400

    Add verbosity levels

--- node-exporter-borg-stats.py
+++ node-exporter-borg-stats.py
@@ -22,6 +22,7 @@ optp.add_option('-k', '--key', help='Repo key', dest='repokey')
 optp.add_option('-i', '--sshargs', help='Additional arguments to pass to SSH command',dest='sshargs')
 optp.add_option('-m', '--metric',help='Unique metric name. REQUIRED',dest='metric')
 optp.add_option('-v', '--verbose',help="Print output to STDOUT", action='store_true',dest='verbose')
+optp.add_option('-q', '--quiet',help="Run in silent mode", action='store_true',dest='quiet')
 optp.add_option('-n', '--hostname',help='Hostname of repository. REQUIRED', dest='hostname')
 opts, args = optp.parse_args()
 
@@ -41,10 +42,26 @@ metric = opts.metric
 if opts.sshargs is not None:
 	os.environ["BORG_RSH"] = opts.sshargs
 
-if opts.verbose is True:
-	verbose = True
+
+
+''' 
+Verbosity config
+0 = Quiet. All messages including errors suppressed.
+1 = Normal mode. Errors shown.
+2 = Verbose. Everything printed. 
+'''
+
+if opts.verbose is True and opts.quiet is True:
+	sys.exit("Error: Conflicting arguments --quiet and --verbose")
+elif opts.verbose is True and opts.quiet is not True:
+	verbosity = 2
+elif opts.verbose is not True and opts.quiet is True:
+	verbosity = 0
 else:
-	verbose = False
+	verbosity = 1
+	
+
+
 if opts.hostname is None:
 	sys.exit('Hostname required')
 hostname = opts.hostname
@@ -63,7 +80,7 @@ def calc_bytes(value, unit):
 def print_prom(file, hostname, archive, parameter, value):
     file.write(parameter+"{host=\""+hostname+"\", archive=\""+archive+"\"} "+str(value)+"\n")
 
-if verbose:
+if verbosity >= 1:
 	print "Updating Prometheus for Borg Repository " + repository
 
 tmp_file_name = textfile_collector_dir+"/"+metric+".tmp"
@@ -74,21 +91,27 @@ proc_borg_list = subprocess.Popen(cmd_borg_list, stdout=subprocess.PIPE, stderr=
 proc_borg_list.wait()
 
 if proc_borg_list.returncode != 0:
-	for line in proc_borg_list.stderr.readlines():
-		m = re.search("passphrase supplied .* is incorrect.", line)
-		if m is not None:
-			print("ERROR: Incorrect Borg repokey for repo " + repository)
+	if verbosity >= 1:
+		for line in proc_borg_list.stderr.readlines():
+			m = re.search("passphrase supplied .* is incorrect.", line)
+			if m is not None:
+				sys.exit("ERROR: Incorrect Borg repokey for repo " + repository)
+					
 
-		m = re.search("Failed to create/acquire the lock", line)
-		if m is not None:
-			sys.exit("ERROR: Unable to obtain lock on repository")
-		else:
-			print(line)
+			m = re.search("Failed to create/acquire the lock", line)
+			if m is not None:
+				sys.exit("ERROR: Unable to obtain lock on repository")
+
+			else:
+				print(line)
+	else:
+		sys.exit();
 
 archives = {}
 count_archives = 0
 for archive in proc_borg_list.stdout.readlines():
-	#print "Raw: "+archive
+	if verbosity == 2:
+		print(archive)
 	m=re.search(r"^(?P<archive>\S+)-(?P<date>\d{4}-\d{2}-\d{2})\S+(?P<time>\d{2}:\d{2}:\d{2})\S*\s", archive)
 	m2=re.search(r"^(?P<name>\S+)\s", archive)
 	if m is not None:
@@ -105,7 +128,7 @@ for archive in proc_borg_list.stdout.readlines():
 			# No archive exists for this name, we add it.
 			archives[m.group('archive')]=this_archive
 
-if verbose:
+if verbosity >= 1:
 	print("Repo " + repository +": " + str(len(archives))+" different archives")
 	print("Repo " + repository +": " + str(count_archives)+" archives in total")
 
@@ -117,7 +140,7 @@ for archive,value in archives.iteritems():
 	archive_name = value['name']
 	archive_datetime=value['datetime']
 
-	if verbose:
+	if verbosity == 2:
 		print("Repo " + repository  + ": updating "+archive+" from archive "+archive_name)
 		print_prom(tmp_file, hostname, archive, "borg_backup_last_update_"+metric, time.mktime(archive_datetime.timetuple()))
 		print_prom(tmp_file, hostname, archive, "borg_backup_age_"+metric, time.time() - time.mktime(archive_datetime.timetuple()))
@@ -129,6 +152,7 @@ for archive,value in archives.iteritems():
 
 	#Quit without updating file if any errors occur while running borg info
 	if proc_borg_info.returncode != 0:
+		if verbosity >= 1:
 			for line in proc_borg_list.stderr.readlines():
 					m = re.search("Broken pipe", line)
 					if m is not None:
@@ -137,9 +161,12 @@ for archive,value in archives.iteritems():
 					else:
 							print(line)
 							sys.exit();
+		else:
+			sys.exit()
 
 for line in proc_borg_info.stdout.readlines():
-	print line
+	if verbosity == 2:
+		print line
 	m=re.search("Number of files: (?P<files_number>\d*)", line)
 	if m:
 		print_prom(tmp_file, hostname, archive, "borg_backup_files_"+metric, m.group('files_number'))
