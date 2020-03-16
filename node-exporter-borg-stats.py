cfe0c9700fb5bc31856b2829b368777f1928f60f
Daniel Blazejewski <dan@backups01.home.danielblazejewski.com>
2020-03-16 13:27:59 -0400

    Refactor to move away from .ini file to command line args

--- node-exporter-borg-stats.py
+++ node-exporter-borg-stats.py
@@ -11,193 +11,158 @@ import thread
 import threading
 import multiprocessing
 from datetime import datetime
+from optparse import OptionParser
 
-
-configfile="/etc/node-exporter-borg-stats.ini"
 textfile_collector_dir="/var/lib/prometheus/node-exporter"
 
+
+optp = OptionParser()
+optp.add_option('-r', '--repository', help='Select repository to update from', dest='repository')
+optp.add_option('-k', '--key', help='Repo key', dest='repokey')
+optp.add_option('-i', '--sshargs', help='Additional arguments to pass to SSH command',dest='sshargs')
+optp.add_option('-m', '--metric',help='Unique metric name. REQUIRED',dest='metric')
+optp.add_option('-v', '--verbose',help="Print output to STDOUT", action='store_true',dest='verbose')
+optp.add_option('-n', '--hostname',help='Hostname of repository. REQUIRED', dest='hostname')
+opts, args = optp.parse_args()
+
+if opts.repository is None:
+	sys.exit('Repository must be specified')
+repository = opts.repository
+
+if opts.repokey is None:
+	sys.exit('Repokey must be specified')
+repokey = opts.repokey
+os.environ["BORG_PASSPHRASE"] = repokey
+
+if opts.metric is None:
+	sys.exit('Unique metric name must be specified')
+metric = opts.metric
+
+if opts.sshargs is not None:
+	os.environ["BORG_RSH"] = opts.sshargs
+
+if opts.verbose is True:
+	verbose = True
+else:
+	verbose = False
+if opts.hostname is None:
+	sys.exit('Hostname required')
+hostname = opts.hostname
+
 def calc_bytes(value, unit):
-	if unit=='kB':
-		return value * 1024
-	elif unit=='MB':
-		return value * 1024 * 1024
-	elif unit=='GB':
-		return value * 1024 * 1024 * 1024
-	elif unit=='TB':
-		return value * 1024 * 1024 * 1024 * 1024
+    if unit=='kB':
+        return value * 1024
+    elif unit=='MB':
+        return value * 1024 * 1024
+    elif unit=='GB':
+        return value * 1024 * 1024 * 1024
+    elif unit=='TB':
+        return value * 1024 * 1024 * 1024 * 1024
+
 
 def print_prom(file, hostname, archive, parameter, value):
     file.write(parameter+"{host=\""+hostname+"\", archive=\""+archive+"\"} "+str(value)+"\n")
 
-def borg_repo_check(**kwargs):
-#def borg_repo_check(repo, repokey, metric_name, sshargs):
-	borg_repo = None
-	borg_repo_key = None
-	metric_name = None
-	hostname = None
-	sshargs = None
+if verbose:
+	print "Updating Prometheus for Borg Repository " + repository
 
-	for key, value in kwargs.items():
-		if key == "repo":
-			borg_repo=value
-		if key == "repokey":
-			borg_repo_key=value
-		if key == "metric_name":
-			metric_name=value
-		if key == "hostname":
-			hostnameval=value
-		if key == "sshargs":
-			sshargs=value
+tmp_file_name = textfile_collector_dir+"/"+metric+".tmp"
+prom_file_name = textfile_collector_dir+"/"+metric+".prom"
 
-	if borg_repo is None:
-		print("ERROR: Borg Repo is required.")
-		return
+cmd_borg_list=["borg","list", repository]
+proc_borg_list = subprocess.Popen(cmd_borg_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+proc_borg_list.wait()
 
-	if borg_repo_key is None:
-		print("ERROR: Repokey is required.")
-		return
-	else:
-		os.environ["BORG_PASSPHRASE"] = borg_repo_key
-
-	if hostnameval is None:
-		hostname=socket.gethostname()
-	else:
-		hostname=hostnameval
-
-	if metric_name is None:
-		print("ERROR: metric_name is required.")
-		return
-
-	if sshargs is not None:
-		os.environ["BORG_RSH"] = sshargs
-
-
-	tmp_file_name = textfile_collector_dir+"/"+metric_name+".tmp"
-	prom_file_name = textfile_collector_dir+"/"+metric_name+".prom"
-
-	cmd_borg_list=["borg","list", borg_repo]
-	proc_borg_list = subprocess.Popen(cmd_borg_list, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	proc_borg_list.wait()
-
-	if proc_borg_list.returncode != 0:
-		for line in proc_borg_list.stderr.readlines():
-			m = re.search("passphrase supplied .* is incorrect.", line)
-			if m is not None:
-				print("ERROR: Incorrect Borg repokey for repo " + borg_repo)
-			else:
-				print(line)
-		return
-
-
-
-	archives = {}
-	count_archives = 0
-
-	for archive in proc_borg_list.stdout.readlines():
-		#print "Raw: "+archive
-		m=re.search(r"^(?P<archive>\S+)-(?P<date>\d{4}-\d{2}-\d{2})\S+(?P<time>\d{2}:\d{2}:\d{2})\S*\s", archive)
-		m2=re.search(r"^(?P<name>\S+)\s", archive)
+if proc_borg_list.returncode != 0:
+	for line in proc_borg_list.stderr.readlines():
+		m = re.search("passphrase supplied .* is incorrect.", line)
 		if m is not None:
-			count_archives = count_archives+1
+			print("ERROR: Incorrect Borg repokey for repo " + repository)
 
-			this_archive={'name':m2.group('name'), 'datetime':datetime.strptime(m.group('date')+" "+m.group('time'), '%Y-%m-%d %H:%M:%S')}
+		m = re.search("Failed to create/acquire the lock", line)
+		if m is not None:
+			sys.exit("ERROR: Unable to obtain lock on repository")
+		else:
+			print(line)
 
-			if m.group('archive') in archives:
-				# An archive with the same name exists, we need to check which is the more recent
-				if archives[m.group('archive')]['datetime']<this_archive['datetime']:
-					archives[m.group('archive')]=this_archive
+archives = {}
+count_archives = 0
+for archive in proc_borg_list.stdout.readlines():
+	#print "Raw: "+archive
+	m=re.search(r"^(?P<archive>\S+)-(?P<date>\d{4}-\d{2}-\d{2})\S+(?P<time>\d{2}:\d{2}:\d{2})\S*\s", archive)
+	m2=re.search(r"^(?P<name>\S+)\s", archive)
+	if m is not None:
+		count_archives = count_archives+1
 
-			else:
-				# No archive exists for this name, we add it.
+		this_archive={'name':m2.group('name'), 'datetime':datetime.strptime(m.group('date')+" "+m.group('time'), '%Y-%m-%d %H:%M:%S')}
+
+		if m.group('archive') in archives:
+			# An archive with the same name exists, we need to check which is the more recent
+			if archives[m.group('archive')]['datetime']<this_archive['datetime']:
 				archives[m.group('archive')]=this_archive
 
-	print("Repo " + borg_repo +": " + str(len(archives))+" different archives")
-	print("Repo " + borg_repo +": " + str(count_archives)+" archives in total")
+		else:
+			# No archive exists for this name, we add it.
+			archives[m.group('archive')]=this_archive
 
-	#Create empty tmp file
-	tmp_file = open(tmp_file_name,"w")
-
-	for archive, value in archives.iteritems():
-		archive_name = value['name']
-		archive_datetime=value['datetime']
-
-		print("Repo " + borg_repo  + ": upating "+archive+" from archive "+archive_name)
-
-		print_prom(tmp_file, hostname, archive, "borg_backup_last_update_"+metric_name, time.mktime(archive_datetime.timetuple()))
-		print_prom(tmp_file, hostname, archive, "borg_backup_age_"+metric_name, time.time() - time.mktime(archive_datetime.timetuple()))
-
-		cmd_borg_info=["borg","info",borg_repo+"::"+archive_name]
-		proc_borg_info = subprocess.Popen(cmd_borg_info, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		proc_borg_info.wait()
+if verbose:
+	print("Repo " + repository +": " + str(len(archives))+" different archives")
+	print("Repo " + repository +": " + str(count_archives)+" archives in total")
 
 
-		#Quit without updating file if any errors occur while running borg info
-        	if proc_borg_info.returncode != 0:
-                	for line in proc_borg_list.stderr.readlines():
-                        	m = re.search("Broken pipe", line)
-                        	if m is not None:
-                                	print("ERROR: Broken pipe while connecting to " + borg_repo)
-                        	else:
-                                	print(line)
-          		return
+# Prepare temporary file
+tmp_file = open(tmp_file_name,"w+")
 
-		for line in proc_borg_info.stdout.readlines():
-			m=re.search("Number of files: (?P<files_number>\d*)", line)
-			if m:
-				print_prom(tmp_file, hostname, archive, "borg_backup_files_"+metric_name, m.group('files_number'))
+for archive,value in archives.iteritems():
+	archive_name = value['name']
+	archive_datetime=value['datetime']
 
-			m=re.search("Chunk index:\s*(?P<chunk_index_unique>\d*)\s*(?P<chunk_index_total>\d*)", line)
-			if m:
-				print_prom(tmp_file, hostname, archive, "borg_backup_chunks_unique_"+metric_name, m.group('chunk_index_unique'))
-				print_prom(tmp_file, hostname, archive, "borg_backup_chunks_total_"+metric_name, m.group('chunk_index_total'))
+	if verbose:
+		print("Repo " + repository  + ": updating "+archive+" from archive "+archive_name)
+		print_prom(tmp_file, hostname, archive, "borg_backup_last_update_"+metric, time.mktime(archive_datetime.timetuple()))
+		print_prom(tmp_file, hostname, archive, "borg_backup_age_"+metric, time.time() - time.mktime(archive_datetime.timetuple()))
 
-			m=re.search("This archive:\s*(?P<original_size>\d*.\d*)\s(?P<original_size_unit>[A-Za-z]{2})\s*(?P<compressed_size>\d*.\d*)\s(?P<compressed_size_unit>[A-Za-z]{2})\s*(?P<total_size>\d*.\d*)\s(?P<total_size_unit>[A-Za-z]{2})", line)
-			if m:
-				print_prom(tmp_file, hostname, archive, "borg_backup_last_size_"+metric_name, calc_bytes(float(m.group('original_size')), m.group('original_size_unit')))
-				print_prom(tmp_file, hostname, archive, "borg_backup_last_size_compressed_"+metric_name, calc_bytes(float(m.group('compressed_size')), m.group('compressed_size_unit')))
-				print_prom(tmp_file, hostname, archive, "borg_backup_last_size_dedup_"+metric_name, calc_bytes(float(m.group('total_size')), m.group('total_size_unit')))
+	#Execute borg info command
+	cmd_borg_info=["borg","info",repository+"::"+archive_name]
+	proc_borg_info = subprocess.Popen(cmd_borg_info, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	proc_borg_info.wait()	
 
-			m=re.search("All archives:\s*(?P<original_size>\d*.\d*)\s(?P<original_size_unit>[A-Za-z]{2})\s*(?P<compressed_size>\d*.\d*)\s(?P<compressed_size_unit>[A-Za-z]{2})\s*(?P<total_size>\d*.\d*)\s(?P<total_size_unit>[A-Za-z]{2})", line)
-			if m:
-				print_prom(tmp_file, hostname, archive, "borg_backup_total_size_"+metric_name, calc_bytes(float(m.group('original_size')), m.group('original_size_unit')))
-				print_prom(tmp_file, hostname, archive, "borg_backup_total_size_compressed_"+metric_name, calc_bytes(float(m.group('compressed_size')), m.group('compressed_size_unit')))
-				print_prom(tmp_file, hostname, archive, "borg_backup_total_size_dedup_"+metric_name, calc_bytes(float(m.group('total_size')), m.group('total_size_unit')))
+	#Quit without updating file if any errors occur while running borg info
+	if proc_borg_info.returncode != 0:
+			for line in proc_borg_list.stderr.readlines():
+					m = re.search("Broken pipe", line)
+					if m is not None:
+							print("ERROR: Broken pipe while connecting to " + borg_repo)
+							sys.exit();
+					else:
+							print(line)
+							sys.exit();
 
-	print("Repo " + borg_repo + ": Complete.")
-	tmp_file.close()
-	shutil.move(tmp_file_name, prom_file_name)
+for line in proc_borg_info.stdout.readlines():
+	print line
+	m=re.search("Number of files: (?P<files_number>\d*)", line)
+	if m:
+		print_prom(tmp_file, hostname, archive, "borg_backup_files_"+metric, m.group('files_number'))
 
+	m=re.search("Chunk index:\s*(?P<chunk_index_unique>\d*)\s*(?P<chunk_index_total>\d*)", line)
+	if m:
+		print_prom(tmp_file, hostname, archive, "borg_backup_chunks_unique_"+metric, m.group('chunk_index_unique'))
+		print_prom(tmp_file, hostname, archive, "borg_backup_chunks_total_"+metric, m.group('chunk_index_total'))
 
+	m=re.search("This archive:\s*(?P<original_size>\d*.\d*)\s(?P<original_size_unit>[A-Za-z]{2})\s*(?P<compressed_size>\d*.\d*)\s(?P<compressed_size_unit>[A-Za-z]{2})\s*(?P<total_size>\d*.\d*)\s(?P<total_size_unit>[A-Za-z]{2})", line)
+	if m:
+		print_prom(tmp_file, hostname, archive, "borg_backup_last_size_"+metric, calc_bytes(float(m.group('original_size')), m.group('original_size_unit')))
+		print_prom(tmp_file, hostname, archive, "borg_backup_last_size_compressed_"+metric, calc_bytes(float(m.group('compressed_size')), m.group('compressed_size_unit')))
+		print_prom(tmp_file, hostname, archive, "borg_backup_last_size_dedup_"+metric, calc_bytes(float(m.group('total_size')), m.group('total_size_unit')))
 
-config = configparser.ConfigParser()
+	m=re.search("All archives:\s*(?P<original_size>\d*.\d*)\s(?P<original_size_unit>[A-Za-z]{2})\s*(?P<compressed_size>\d*.\d*)\s(?P<compressed_size_unit>[A-Za-z]{2})\s*(?P<total_size>\d*.\d*)\s(?P<total_size_unit>[A-Za-z]{2})", line)
+	if m:
+		print_prom(tmp_file, hostname, archive, "borg_backup_total_size_"+metric, calc_bytes(float(m.group('original_size')), m.group('original_size_unit')))
+		print_prom(tmp_file, hostname, archive, "borg_backup_total_size_compressed_"+metric, calc_bytes(float(m.group('compressed_size')), m.group('compressed_size_unit')))
+		print_prom(tmp_file, hostname, archive, "borg_backup_total_size_dedup_"+metric, calc_bytes(float(m.group('total_size')), m.group('total_size_unit')))
 
+if verbose:
+	print("Repo " + repository + ": Complete.")
 
-try:
-	with open (configfile) as f:
-		config.read_file(f)
-except IOError:
-	raise ValueError, "config file does not exist"
-
-
-
-jobs = []
-for metric in config.sections():
-	crepo = ""
-	crepokey = ""
-	csshargs = ""
-	hostname = ""
-	if config[metric]['repo']:
-		crepo = config[metric]['repo']
-	if config[metric]['repokey']:
-		crepokey = config[metric]['repokey']
-	if config[metric]['sshargs']:
-		csshargs = config[metric]['sshargs']
-	if config[metric]['hostname']:
-		chostname = config[metric]['hostname']
-	kwargsX = {"repo": crepo, "repokey": crepokey, "metric_name": metric, "sshargs" :csshargs, "hostname" : chostname}
-
-
-	p = multiprocessing.Process(target=borg_repo_check, args=(), kwargs=kwargsX)
-	p.start()
-	time.sleep(1)
-
+tmp_file.close()
+shutil.move(tmp_file_name, prom_file_name)
